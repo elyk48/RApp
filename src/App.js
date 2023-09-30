f1bb645369152a1ee7528646e443dac55aa21e0e
elyeskabous <elyes.kabous@gmail.com>
2023-09-30 16:35:30 +0100

    chagin the app.js code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
-import Heading from './Heading';
-
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Promo from './components/Promo';
 function App() {
   return(
     <div className="App"> 
-      
-      <Heading /> 
+
+      <Nav/>
+      <Intro1></Intro1>
+      <Intro2></Intro2>
+      <Intro3></Intro3>
+      <Promo></Promo>
+      <Footer/>
 
     </div> 
 
